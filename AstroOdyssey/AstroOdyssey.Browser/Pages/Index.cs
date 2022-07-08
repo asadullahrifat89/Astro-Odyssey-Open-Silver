2293a4a997ec37cd2c89d7b9c045f04e718c6a32
Asadullah Rifat <asadullah.rifat@selise.ch>
2022-07-08 18:31:58 +0600

    Add project files.

---
+++ AstroOdyssey/AstroOdyssey.Browser/Pages/Index.cs
@@ -0,0 +1,26 @@
+﻿using AstroOdyssey.Browser.Interop;
+using DotNetForHtml5;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.JSInterop;
+
+namespace AstroOdyssey.Browser.Pages
+{
+    [Route("/")]
+    public class Index : ComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder __builder)
+        {
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Cshtml5Initializer.Initialize(new UnmarshalledJavaScriptExecutionHandler(JSRuntime));
+            Program.RunApplication();
+        }
+
+        [Inject]
+        private IJSRuntime JSRuntime { get; set; }
+    }
+}
---
+++ AstroOdyssey/AstroOdyssey.Simulator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using OpenSilver.Simulator;
+using System;
+
+namespace AstroOdyssey.Simulator
+{
+    internal static class Startup
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            return SimulatorLauncher.Start(typeof(App));
+        }
+    }
+}
