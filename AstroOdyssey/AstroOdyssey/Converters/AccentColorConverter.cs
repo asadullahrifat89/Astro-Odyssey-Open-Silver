94b70eed28693a15c22eb28c58fdc347c087ca47
Asadullah Rifat <asadullah.rifat@selise.ch>
2022-07-08 18:36:26 +0600

    TASK: Styles added for UI

---
+++ AstroOdyssey/AstroOdyssey/Converters/AccentColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace AstroOdyssey
+{
+    public class AccentColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is SolidColorBrush)
+            {
+                Color oldColor = ((SolidColorBrush)value).Color;
+                byte newR = oldColor.R > 40 ? (byte)(oldColor.R - 40) : (byte)0;
+                byte newG = oldColor.G > 40 ? (byte)(oldColor.G - 40) : (byte)0;
+                byte newB = oldColor.B > 40 ? (byte)(oldColor.B - 40) : (byte)0;
+                Color newColor = Color.FromArgb(oldColor.A, newR, newG, newB);
+                return new SolidColorBrush(newColor);
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AstroOdyssey/AstroOdyssey/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace AstroOdyssey
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool flag = false;
+            if (value is bool)
+            {
+                flag = (bool)value;
+            }
+            else if (value is bool?)
+            {
+                bool? nullable = (bool?)value;
+                flag = nullable.HasValue ? nullable.Value : false;
+            }
+
+            bool inverse = (parameter as string) == "inverse";
+
+            if (inverse)
+            {
+                return flag ? Visibility.Collapsed : Visibility.Visible;
+            }
+            else
+            {
+                return flag ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
